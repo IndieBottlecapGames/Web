96333f6dbc906a18543f37936ff8f1592309474f
phochs <me.accounts+github@runbox.com>
2017-07-24 17:28:29 +0200

    Initial website

---
+++ public_html/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection" />
+	<link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>IndieBottlecap Games</title>
+</head>
+<body>
+	<header>
+		<nav class="light-blue darken-3">
+			<div class="container">
+				<div class="nav-wrapper">
+					<a href="#" class="brand-logo">IndieBottlecap Games</a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a href="#">Games</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Team</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<div class="parallax-container">
+			<div class="parallax"><img src="img/header.jpg"></div>
+		</div>
+		<div class="section blue-grey darken-1">
+			<div class="row container">
+				<div class="col l4 m6 s12">
+                    <div id="promo-wacky" class="center promo z-depth-4">
+                        &nbsp;
+                        <div class="light-blue darken-3">
+                            <h4 class="title">WackyRaces</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l4 m6 s12">
+                    <div id="promo-skillshot" class="center promo z-depth-4">
+                        &nbsp;
+                        <div class="light-blue darken-3">
+                            <h4 class="title">Skillshot</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l4 m6 s12">
+                    <div id="promo-wacky" class="center promo z-depth-4">
+                        &nbsp;
+                        <div class="light-blue darken-3">
+                            <h4 class="title">Another Game</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="divider grey darken-2"></div>
+                <h3>About</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus et justo id porta. Morbi eu eleifend metus. Nullam sollicitudin eros sed euismod blandit. Phasellus nibh velit, commodo et suscipit ac, pretium at neque. Integer cursus elementum erat. Cras consectetur sem molestie rhoncus posuere. Etiam eleifend dictum sodales. Vivamus nec libero quis sapien ultricies congue. Pellentesque mattis, ipsum et efficitur dapibus, sem metus facilisis enim, rhoncus porttitor sem leo et massa. Sed sit amet nisl ac libero cursus ultricies in ut ipsum. Phasellus in tincidunt turpis, ac commodo nulla.
+                </p><p>
+                    In ultricies quam augue, ac scelerisque sapien sodales et. Ut metus neque, efficitur vitae volutpat vel, tristique at tellus. Sed varius neque ac ante maximus fringilla. Aenean sit amet ornare odio, quis dapibus mi. Ut molestie tincidunt fringilla. Vivamus condimentum elementum gravida. Mauris tristique, quam in cursus consectetur, magna ante mollis lectus, id dictum lectus diam in mi. Duis rutrum massa lacus, ac consequat augue lacinia ut. In finibus quam sed erat tristique, a interdum purus pellentesque. Vivamus varius vestibulum massa.
+                </p>
+                <img src="img/discord.jpg" height="70" />
+                <img src="img/twitch.jpeg" height="70" />
+                <img src="img/github.png" height="70" />
+                <div class="divider grey darken-2"></div>
+                <h3>Team</h3>
+                <div class="row">
+                    <div class="col s3 center member">
+                        <img class="circle" src="img/team/user.png" width="120" />
+                        <h5>Arachnenet</h5>
+                        <h6 class="grey-text text-lighten-2">(Audio engineer)</h6>
+                    </div>
+                    <div class="col s3 center member">
+                        <img class="circle" src="img/team/user.png" width="120" />
+                        <h5>Mythiril</h5>
+                        <h6 class="grey-text text-lighten-2">(Story writer)</h6>
+                    </div>
+                    <div class="col s3 center member">
+                        <img class="circle" src="img/team/user.png" width="120" />
+                        <h5>phochs</h5>
+                        <h6 class="grey-text text-lighten-2">(Web designer)</h6>
+                    </div>
+                    <div class="col s3 center member">
+                        <img class="circle" src="img/team/user.png" width="120" />
+                        <h5>Sagoza</h5>
+                        <h6 class="grey-text text-lighten-2">(Project lead/artist)</h6>
+                    </div>
+                    <div class="col s3 center member">
+                        <img class="circle" src="img/team/user.png" width="120" />
+                        <h5>Silver</h5>
+                        <h6 class="grey-text text-lighten-2">(Artist)</h6>
+                    </div>
+                </div>
+            </div>
+		</div>
+	</main>
+	<footer class="page-footer grey darken-3">
+		<div class="container">
+			<div class="row">
+				<div class="col l6 s12">
+					<img class="footer-img" src="img/indiebottlecap.png" /> <h5 class="white-text">IndieBottlecapGames.com</h5>
+					<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+				</div>
+				<div class="col l4 offset-l2 s12">
+					<h5 class="white-text">Links</h5>
+					<ul>
+						<li><a class="grey-text text-lighten-3" href="#!">Copyright</a></li>
+						<li><a class="grey-text text-lighten-3" href="#!">Links</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="footer-copyright grey darken-4">
+			<div class="container">
+				© 2017 IndieBottlecap Games
+			</div>
+		</div>
+	</footer>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('.parallax').parallax();
+		});
+	</script>
+</body>
+</html>
